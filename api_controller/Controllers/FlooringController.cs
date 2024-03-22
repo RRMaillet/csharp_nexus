171d77a6e3315b5964da8eccee7fc6cb84a76068
Robert <rrmaillet@gmail.com>
2024-03-22 11:10:35 -0400

    Added FlooringController and deleted the demo Weather Forcasting demo files

---
+++ api_controller/Controllers/FlooringController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_controller.Controllers
+
+{
+    [ApiController]
+    public class FlooringController : ControllerBase
+    {
+        [HttpGet]
+        public string GetFloors()
+        {
+            return "Reading all the floors...";
+        }
+
+        [HttpGet]
+        public string GetFloorById(int id)
+        {
+            return $"Reading Floor with ID {id}";
+        }
+
+        [HttpGet]
+        public string UpdateFloor(int id)
+        {
+            return $"Updating Floor with ID {id}";
+        }
+
+        [HttpGet]
+        public string DeleteFloor(int id)
+        {
+            return $"Deleting Floor with ID {id}";
+        }
+    }
+}
+
+
--- api_controller/obj/Release/net8.0/api_controller.AssemblyInfo.cs
+++ api_controller/obj/Release/net8.0/api_controller.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api_controller")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2afc1a914bd874f4fd9de9a9c09875cbc67806b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05db1c03d1930807d501337306d1f52f1a5e341a")]
 [assembly: System.Reflection.AssemblyProductAttribute("api_controller")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api_controller")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
